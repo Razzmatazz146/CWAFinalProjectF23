cd4768b05f18d929ec2ae57e6d12010a5be08f59
Razzmatazz146 <le1935607@crc-lennox.qc.ca>
2023-11-29 13:52:31 -0500

    All the songs work, have jsons.

--- my-react-dashboard/src/Views/SpotifyLibrary.jsx
+++ my-react-dashboard/src/Views/SpotifyLibrary.jsx
@@ -11,8 +11,8 @@ function SpotifyLibrary() {
         <div className="row">
           <div className="col">
             <div className="card">
-              <h5>Title</h5>
-              <h5>Artist</h5>
+              <h5>SLOW DANCING IN THE DARK</h5>
+              <h5>Joji</h5>
               <AudioPlayer 
               autoPlay={false}
               src="https://p.scdn.co/mp3-preview/1aa59cfc4e15a45403fd35eb72ce62acd8a45f58?cid=268afd9ec3854be8833458e463e1682c"/>
@@ -20,8 +20,8 @@ function SpotifyLibrary() {
           </div>
           <div className="col">
             <div className="card">
-            <h5>Title</h5>
-              <h5>Artist</h5>
+            <h5>Demons</h5>
+              <h5>Joji</h5>
               <AudioPlayer 
               autoPlay={false}
               src="https://p.scdn.co/mp3-preview/8dc978c39f1f2856a9173060754b73f5bc068bc0?cid=268afd9ec3854be8833458e463e1682c"/>
@@ -31,24 +31,40 @@ function SpotifyLibrary() {
         <div className="row">
           <div className="col">
             <div className="card">
-              Song 3
+              <h5>Glimpse of Us</h5>
+              <h5>Joji</h5>
+            <AudioPlayer 
+              autoPlay={false}
+              src="https://p.scdn.co/mp3-preview/12e4a7ef3f1051424e6e282dfba83fe8448e122f?cid=268afd9ec3854be8833458e463e1682c"/>
             </div>
           </div>
           <div className="col">
             <div className="card">
-              Song 4
+            <h5>Sanctuary</h5>
+              <h5>Joji</h5>
+            <AudioPlayer 
+              autoPlay={false}
+              src="https://p.scdn.co/mp3-preview/b3c2037a7cd4745177be546b5e91a3d1e663ce9f?cid=268afd9ec3854be8833458e463e1682c"/>
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col">
             <div className="card">
-              Song 5
+            <h5>YEAH RIGHT</h5>
+              <h5>Joji</h5>
+            <AudioPlayer 
+              autoPlay={false}
+              src="https://p.scdn.co/mp3-preview/a5583f1e308e542367e722ed879b93bdb4e25f8f?cid=268afd9ec3854be8833458e463e1682c"/>
             </div>
           </div>
           <div className="col">
             <div className="card">
-              Song 6
+            <h5>Like You Do</h5>
+              <h5>Joji</h5>
+            <AudioPlayer 
+              autoPlay={false}
+              src="https://p.scdn.co/mp3-preview/b6264d2126b08a8219df6502c8b613ee9ebe5577?cid=268afd9ec3854be8833458e463e1682c"/>
             </div>
           </div>
         </div>
