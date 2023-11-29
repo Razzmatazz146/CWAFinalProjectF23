746842315891d2883cd32d3cb6ba39ad95d7982d
Willuku <le1952006@crc-lennox.qc.ca>
2023-11-29 13:38:49 -0500

    Started work on LocalLibrary component

--- my-react-dashboard/src/Views/LocalLibrary.jsx
+++ my-react-dashboard/src/Views/LocalLibrary.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import LocalTrack from '../Components/LocalTrack'
+import { localTracks } from '../data/localTracks'
 
 function LocalLibrary() {
   return (
     <div>
-      
+      <h1>Local Music</h1>
+      {localTracks.map(track => {
+        return (
+          <LocalTrack name={track.name} artist={track.artist} duration={track.duration} source={track.source} listens={2}/>
+        )
+      })}
+      <button className="btn btn-secondary">Back</button>
     </div>
   )
 }
---
+++ my-react-dashboard/src/data/localTracks.js
@@ -0,0 +1,37 @@
+export const localTracks = [
+    {
+        id: 1,
+        name: "Boss Battle 6",
+        author: "nene",
+        duration: "3:29",
+        source: "../assets/BossBattle6MetalV2.wav"
+    },
+    {
+        id: 2,
+        name: "Boss Battle 2",
+        author: "nene",
+        duration: "0:39",
+        source: "../assets/boss_battle_#2_metal_opening.wav"
+    },
+    {
+        id: 3,
+        name: "Boss Battle 7",
+        author: "nene",
+        duration: "0:46",
+        source: "../assets/boss_battle_07.wav"
+    },
+    {
+        id: 4,
+        name: "Fast Fight",
+        author: "Ville Nousiainen",
+        duration: "0:26",
+        source: "../assets/fight.wav"
+    },
+    {
+        id: 5,
+        name: "JRPG Battle",
+        author: "HydroGene",
+        duration: "1:55",
+        source: "../assets/jrpg_battle_full.wav"
+    }
+]
