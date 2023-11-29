00ab6006de4d5b54efe02a807717091164ae6b0a
Willuku <le1952006@crc-lennox.qc.ca>
2023-11-29 11:30:14 -0500

    Set up routing on main.jsx

--- my-react-dashboard/src/main.jsx
+++ my-react-dashboard/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Views/Home.jsx'
+import LocalLibrary from './Views/LocalLibrary.jsx'
+import SpotifyLibrary from './Views/SpotifyLibrary.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />}/>
+      <Route path='/' element={<Home />}/>
+      <Route path='/local' element={<LocalLibrary />} />
+      <Route path='/spotify' element={<SpotifyLibrary />} />
+    </Routes>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
