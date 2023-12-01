239c038fe74403424aa539d431559e1f73cd3944
Willuku <le1952006@crc-lennox.qc.ca>
2023-11-30 19:51:37 -0500

    LocalTrack.jsx fix

--- my-react-dashboard/src/Components/LocalTrack.jsx
+++ my-react-dashboard/src/Components/LocalTrack.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function LocalTrack(props) {    
     return (
         <div className='row card my-3 bg-light border border-info'>
-            <div className="container">
+            <div className="container-fluid">
                 <div className="row">
                     <div className="col fw-bold">{props.name}</div>
                     <div className="col">{props.artist}</div>
                     <div className="col text-secondary">{props.duration}</div>
-                    <div className="col btn btn-info text-white" onClick={playTrack}>Play ▶️</div>
+                    <div className="col btn btn-info text-white">Play ▶️</div>
                     <div className="col">Played {props.listens} times.</div>
                 </div>
             </div>
