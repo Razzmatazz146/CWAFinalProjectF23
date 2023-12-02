e5f7bddf525e8763c50e0426c439d56bc5574c45
Razzmatazz146 <denispletourneau@hotmail.com>
2023-12-02 05:59:50 -0500

    added icon and logo for copyright. Added states to make results and player visible/invisible

--- my-react-dashboard/src/Views/SpotifyLibrary.jsx
+++ my-react-dashboard/src/Views/SpotifyLibrary.jsx
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import SearchResults from '../Components/SearchResults'
 import { performSearch } from '../data/spotify';
 import MusicPlayer from '../Components/MusicPlayer';
+import spotifyIcon from '../assets/Spotify_Icon_CMYK_White.png';
+import spotifyLogo from '../assets/Spotify_Logo_CMYK_Black.png';
 
 function SpotifyLibrary() {
+  const [isVisibleResults, setIsVisibleResults] = useState(false);
+  const [isVisiblePlayer, setIsVisiblePlayer] = useState(false);
   const [preview, setPreview] = useState('');
   const [keyword, setKeyword] = useState('');
   const [searchResults, setSearchResults] = useState([]);
@@ -26,32 +30,46 @@ function SpotifyLibrary() {
 
   const handleSearch = async () => {
     const results = await performSearch(keyword);
+    setIsVisibleResults(true);
     setSearchResults(results);
   };
 
   return (
-    <div style={{ maxHeight: '80vh', marginBottom: '8vh', maxWidth: '90vw' }}>
-      <a className="btn btn-secondary btn-border-radius-0 position-fixed top-0 start-0" href="/home">Back</a>
-      <div className="container pt-2">
-        <h1>Search Spotify</h1>
+    <div className="text-center">
+      <div className="container">
+        <a className="btn btn-secondary btn-border-radius-0 position-fixed top-0 start-0" href="/home">Back</a>
+      </div>
+      <div className="container pt-5">
+        <div className="container d-flex justify-content-center align-items-center centered-content">
+          <img src={spotifyLogo} style={{ maxHeight: '70px', marginRight: '10px' }} />
+          <h1>Library</h1>
+        </div>
         <br />
         <div className="input-group input-group-lg mb-3">
           <input type="text" value={keyword} className="form-control" placeholder="Album or Song Name" onChange={onNewSearch} />
           <button className="btn btn-outline-secondary bg-success bg-gradient text-white" type="button"
             id="button-addon2" data-mdb-ripple-init data-mdb-ripple-color="dark" onClick={handleSearch}>
-            Search
+            <div>
+              Search
+            </div>
+            <div>
+              <img src={spotifyIcon} alt="logo" style={{ maxHeight: '24px' }} />
+            </div>
           </button>
         </div>
-        <h3>Search Results</h3>
-        {searchResults.map(track => {
-          return (
-            <div key={track.id}>
-              <SearchResults song={track.song} artist={track.artist} album={track.album} duration={track.duration} playSong={playSong} previewUrl={track.previewUrl} key={track.id} />
-
-            </div>
-          )
-        })}
-        <MusicPlayer currentSong={currentSong.song} url={currentSong.url} artist={currentSong.artist} />
+        {isVisibleResults && <div>
+          <h3>Search Results</h3>
+          {searchResults.map(track => {
+            return (
+              <div key={track.id}>
+                <SearchResults song={track.song} artist={track.artist} album={track.album} duration={track.duration} playSong={playSong} previewUrl={track.previewUrl} key={track.id} />
+              </div>
+            )
+          })}
+        </div>}
+        {isVisiblePlayer&& <div id='musicPlayer'>
+          <MusicPlayer currentSong={currentSong.song} url={currentSong.url} artist={currentSong.artist} />
+        </div>}
       </div>
     </div>
   )
