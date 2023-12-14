10c2e32fb95ef186752cc8872d127d294f372548
Willuku <le1952006@crc-lennox.qc.ca>
2023-12-14 04:08:04 -0500

    She local on my storage

--- my-react-dashboard/src/Components/MusicPlayer.jsx
+++ my-react-dashboard/src/Components/MusicPlayer.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
 function MusicPlayer(props) {
-    return (
-        <div className="container border rounded fixed-bottom bg-dark bg-gradient text-light">
-            <h4>Playing: <span className='fw-bold text-light'>{props.song}</span> by {props.artist}</h4>
-            <div className="container my-1">
-                <AudioPlayer autoPlay={true} src={props.url} volume={0.5} />
-            </div>
-            
-        </div>
-    )
+  const [playCounted, setPlayCounted] = useState(false);
+
+  const handleEnded = () => {
+    if (!playCounted) {
+      if (typeof props.onEnded === 'function') {
+        props.onEnded();
+        setPlayCounted(true);
+      }
+    }
+  };
+
+  return (
+    <div className="container border rounded fixed-bottom bg-dark bg-gradient text-light">
+      <h4>
+        Playing: <span className='fw-bold text-light'>{props.song}</span> by {props.artist}
+      </h4>
+      <div className="container my-1">
+        <AudioPlayer autoPlay={true} src={props.url} volume={0.5} onEnded={handleEnded} />
+      </div>
+    </div>
+  );
 }
 
-export default MusicPlayer
+export default MusicPlayer;
--- my-react-dashboard/src/Views/LocalLibrary.jsx
+++ my-react-dashboard/src/Views/LocalLibrary.jsx
@@ -1,44 +1,80 @@
-import React, { useState } from 'react'
-import LocalTrack from '../Components/LocalTrack'
-import AudioPlayer from 'react-h5-audio-player'
-import 'react-h5-audio-player/lib/styles.css'
-import { localTracks } from '../data/localTracks'
+import React, { useState, useEffect } from 'react';
+import LocalTrack from '../Components/LocalTrack';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import { localTracks } from '../data/localTracks';
+import MusicPlayer from '../Components/MusicPlayer';
 
 function LocalLibrary() {
   const [source, setSource] = useState('');
   const [currentSong, setCurrentSong] = useState({
     name: '',
-    artist: ''
+    artist: '',
   });
 
   const playSong = (url, songDetails) => {
     setSource(url);
     setCurrentSong(songDetails);
+  };
 
-    console.log(url, ", ", songDetails)
-  }
+  const handleSongEnd = () => {
+    const trackName = currentSong.name;
+    const playedTracks = JSON.parse(localStorage.getItem('playedTracks')) || {};
+    playedTracks[trackName] = (playedTracks[trackName] || 0) + 1;
+    localStorage.setItem('playedTracks', JSON.stringify(playedTracks));
+    setCurrentSong({
+      name: '',
+      artist: '',
+    }); 
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem('playedTracks')) {
+      localStorage.setItem('playedTracks', JSON.stringify({}));
+    }
+  }, []);
 
   return (
     <div className='text-center'>
       <h1>Local Music</h1>
-      <div className='px-2 container-fluid' style={{ "paddingBottom": "175px" }}>
-        <div className="row">
-          {localTracks.map(track => {
+      <div className='px-2 container-fluid' style={{ paddingBottom: '175px' }}>
+        <div className='row'>
+          {localTracks.map((track) => {
+            const listens = JSON.parse(localStorage.getItem('playedTracks'))?.[track.name] || 0;
+
             return (
-              <div className="col-6" key={track.id}>
-                <LocalTrack name={track.name} artist={track.artist} duration={track.duration} source={track.source} listens={2} playSong={playSong} />
+              <div className='col-6' key={track.id}>
+                <LocalTrack
+                  name={track.name}
+                  artist={track.artist}
+                  duration={track.duration}
+                  source={track.source}
+                  listens={listens}
+                  playSong={playSong}
+                />
               </div>
-            )
+            );
           })}
         </div>
-        <a className="btn btn-secondary" href='/'>Back</a>
+        <a className='btn btn-secondary' href='/'>
+          Back
+        </a>
       </div>
-      <div className="container border rounded fixed-bottom bg-white">
-        <h4>Playing: <span className='fw-bold'>{currentSong.name}</span> by {currentSong.artist}</h4>
-        <AudioPlayer src={source} preload='none' autoPlay={false}/>
-      </div>
+      {currentSong.name && (
+        <div className='container border rounded fixed-bottom bg-white'>
+          <h4>
+            Playing: <span className='fw-bold'>{currentSong.name}</span> by {currentSong.artist}
+          </h4>
+          <MusicPlayer
+            song={currentSong.name}
+            url={source}
+            artist={currentSong.artist}
+            onEnded={handleSongEnd}
+          />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default LocalLibrary
+export default LocalLibrary;
