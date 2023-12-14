a86aeda0b825fc11b536abddf6624b1e1b560f8e
Razzmatazz146 <denispletourneau@hotmail.com>
2023-12-13 23:05:16 -0500

    Changed home page a bit more to look like Francis'

--- my-react-dashboard/src/Views/Home.jsx
+++ my-react-dashboard/src/Views/Home.jsx
@@ -5,8 +5,18 @@ function Home() {
     <div className="container">
       <div className="container" style={{ paddingTop: '200px' }}>
         <h1>THE BOYS</h1>
-        <a className="btn btn-primary mx-3" href='/local'>Local Library</a>
-        <a className="btn btn-success mx-3" href='/spotify'>Spotify Library</a>
+        <h3>Choose the library you would like to access:</h3>
+        <br />
+        <div className="row">
+          <div className="col">
+            <a className="btn btn-primary mx-3" href='/local'>Local Library</a>
+            <h6>Browse a curated selection</h6>
+          </div>
+          <div className="col">
+            <a className="btn btn-success mx-3" href='/spotify'>Spotify Library</a>
+            <h6>Search for songs using the Spotify API</h6>
+          </div>
+        </div>
       </div>
     </div>
   )
